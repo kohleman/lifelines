314f7a8ed7a7c3e24786d0bf36c5469fa740189b
Manuel Kohler <manuel.kohler@roche.com>
2022-09-19 15:49:11 +0200

    added README.md text

---
+++ README.md
@@ -0,0 +1,10 @@
+# Life lines
+Create a plot where each line represents a day in the life of a human:
+The amount of days is configurable. A 80 year old human experiences 80 * 365 days = 29200 days.
+
+`python plot.py`
+
+Output looks like this:
+
+
+![Life Lines](life_lines.png)
--- src/plot_lines_per_day/plot.py
+++ src/plot_lines_per_day/plot.py
@@ -12,16 +12,22 @@ from matplotlib import cm
 from numpy import linspace
 import random
 
-plt.figure(figsize=(30, 50), dpi=300)
+plt.figure(figsize=(50, 80), dpi=300)
 
 start = 0.0
 stop = 1.0
-number_of_lines = 3000
+
+# represents the days, should be 29200
+number_of_lines = 1000
 cm_subsection = linspace(start, stop, number_of_lines)
 
 random.shuffle(cm_subsection)
 
-colors = [cm.prism(x) for x in cm_subsection]
+# prism color map
+# colors = [cm.prism(x) for x in cm_subsection]
+
+# jet color map
+colors = [cm.jet(x) for x in cm_subsection]
 
 for i, color in enumerate(colors):
     plt.axhline(i, color=color)
